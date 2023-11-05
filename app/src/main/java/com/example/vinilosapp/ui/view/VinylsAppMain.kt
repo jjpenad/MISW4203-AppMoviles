a4f22d9bd68b2ba393e2f3a13431b83a6429f0d2
Juan José Peña Delgado <jj.penad@uniandes.edu.co>
2023-11-05 18:03:47 -0500

    Feature/configurar el servicio en retrofit (#27)
    * Updating correct setup
    
    * Fxing whole setup

---
+++ app/src/main/java/com/example/vinilosapp/ui/view/VinylsAppMain.kt
@@ -0,0 +1,12 @@
+package com.example.vinilosapp.ui.view
+
+import android.app.Application
+import dagger.Component
+import dagger.hilt.android.HiltAndroidApp
+
+// Definition of the Application graph
+
+@HiltAndroidApp
+class VinylsAppMain : Application() {
+
+}
