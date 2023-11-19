26f3050b4f0a152df67781f3662955cb06a10f96
Setroun <sg.moreno1543@uniandes.edu.co>
2023-11-19 11:30:37 -0500

    Feature/crear vista detalle artistas correcion UI (#37)
    * Sprint 3 Deliverable (#31)
    
    * Feature/configurar el servicio en retrofit (#27)
    
    * Updating correct setup
    
    * Fxing whole setup
    
    * Se agregan los cambios relacionados con el recycle view y el catalogo de albumes
    
    * Feature/configurar el servicio en retrofit (#27)
    
    * Updating correct setup
    
    * Fxing whole setup
    
    * Adapting the changes to daggerhilt
    
    * UI fixes for Album listing
    
    * Catalogo albumes y recycle view (#28)
    
    * Se agregan los cambios relacionados con el recycle view y el catalogo de albumes
    
    * Feature/configurar el servicio en retrofit (#27)
    
    * Updating correct setup
    
    * Fxing whole setup
    
    * Adapting the changes to daggerhilt
    
    * UI fixes for Album listing
    
    ---------
    
    Co-authored-by: Setroun <sg.moreno1543@uniandes.edu.co>
    
    * Update to load images with Glide
    
    * implementacion vista detalle de album
    
    * Upload generated apk and UI fix
    
    ---------
    
    Co-authored-by: Setroun <sg.moreno1543@uniandes.edu.co>
    Co-authored-by: Nicolas Rey <nicolasreydh@gmail.com>
    Co-authored-by: Nicolás Rey <n.reyd@uniandes.edu.co>
    
    * Se agregan correcciones a la UI para albumes y se crea la vista para artistas y detalle de artistas, se configura la ArtistAPIClient
    
    * Adding missing dependency injection
    
    ---------
    
    Co-authored-by: Juan José Peña Delgado <jj.penad@uniandes.edu.co>
    Co-authored-by: Nicolas Rey <nicolasreydh@gmail.com>
    Co-authored-by: Nicolás Rey <n.reyd@uniandes.edu.co>

---
+++ app/src/main/java/com/example/vinilosapp/ArtistProvider.kt
@@ -0,0 +1,13 @@
+package com.example.vinilosapp
+
+import com.example.vinilosapp.data.model.Album
+
+class ArtistProvider {
+
+    companion object {
+
+        val ArtistList= listOf<Album>(
+
+        )
+    }
+}
---
+++ app/src/main/java/com/example/vinilosapp/ui/view/AlbumList.kt
@@ -0,0 +1,59 @@
+package com.example.vinilosapp.ui.view
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.vinilosapp.data.model.Album
+import com.example.vinilosapp.ui.view.adapter.AlbumAdapter
+import com.example.vinilosapp.databinding.ActivityMainBinding
+import com.example.vinilosapp.ui.viewmodel.AlbumListViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AlbumList : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: AlbumAdapter
+
+    private val albumListViewModel: AlbumListViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.myToolbar)
+        binding.myToolbar.title="Albums"
+
+        binding.recyclerAlbum
+        initRecyclerView()
+        searchAlbums()
+    }
+
+
+
+
+    private fun initRecyclerView(){
+        adapter = AlbumAdapter()
+        binding.recyclerAlbum.layoutManager = LinearLayoutManager(this)
+        binding.recyclerAlbum.adapter = adapter
+    }
+
+    private fun searchAlbums(){
+        albumListViewModel.albums.observe(this@AlbumList, Observer {
+            val albumList: List<Album>? = it
+            runOnUiThread{
+                adapter.updateList(albumList)
+            }
+        })
+
+        albumListViewModel.getAlbums()
+    }
+
+
+}
