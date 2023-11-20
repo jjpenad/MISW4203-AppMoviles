1efe0f512e8203827fed0cd3438bae6084650896
jj.penad <jj.penad@uniandes.edu.co>
2023-11-19 21:17:46 -0500

    Adding navigation for Collectors

--- app/src/main/java/com/example/vinilosapp/ui/view/adapter/CollectorAdapter.kt
+++ app/src/main/java/com/example/vinilosapp/ui/view/adapter/CollectorAdapter.kt
@@ -8,6 +8,12 @@ import com.example.vinilosapp.data.model.Collector
 
 class CollectorAdapter (private var collectorList:List<Collector>? = emptyList()): RecyclerView.Adapter<CollectorViewholder>(){
 
+    private var onItemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.onItemClickListener = listener
+    }
+
     fun updateList(collectorList: List<Collector>?){
         this.collectorList=collectorList
         notifyDataSetChanged()
@@ -21,8 +27,19 @@ class CollectorAdapter (private var collectorList:List<Collector>? = emptyList()
 
     override fun onBindViewHolder(holder: CollectorViewholder, position: Int) {
         (collectorList?.get(position) ?:null)?.let { holder.render(it) }
+
+        // Set click listener in onBindViewHolder
+        holder.itemView.setOnClickListener {
+            val collectorId = collectorList?.get(position)?.id
+            if (collectorId != null) {
+                onItemClickListener?.onItemClick(collectorId.toInt())
+            }
+        }
     }
 
+    interface OnItemClickListener {
+        fun onItemClick(albumId: Int) // Change the type of ID according to your Album model
+    }
 }
 
 
